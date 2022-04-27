4d7b8cc3517d73e06078f4e7625fac0f41c1d6fa
loganhansel <loganhansel23@gmail.com>
2022-04-27 15:17:22 -0400

    commit tree 4b8ea9ae9b1e5828dce969dc56c53fd89d91a689

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(someValue) {
+    if (someValue >= 42) {
+        return (someValue - 42);
+    } else {
+        return (42 - someValue);
+    }
+}
+
+function distanceFromHqInFeet(someValue) {
+    return (distanceFromHqInBlocks(someValue) * 264);
+}
+
+function distanceTravelledInFeet(value1, value2) {
+    const dist1 = distanceFromHqInFeet(value1);
+    const dist2 = distanceFromHqInFeet(value2);
+    if (dist1 >= dist2) {
+        return dist1 - dist2;
+    } else {
+        return dist2 - dist1;
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    const travel = distanceTravelledInFeet(start, destination);
+    if (travel <= 400) {
+        return 0;
+    } else if (travel <= 2000) {
+        return (((travel - 400) * 2) / 100);
+    } else if (travel <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
